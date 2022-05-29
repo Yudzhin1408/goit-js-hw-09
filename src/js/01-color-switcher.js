8c4360d640eb533711e3663cf6066623fd457b59
Yevhenii Kononets <yudzhin1408@gmail.com>
2022-05-29 16:05:22 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
+const startBtn = document.querySelector('button[data-start]');
+const bodyEl = document.querySelector('body');
+const stopBtn = document.querySelector('button[data-stop]');
 
+
+startBtn.addEventListener('click', startBtnClick);
+stopBtn.addEventListener('click', stopBtnClick);
+
+let timerId = null;
+stopBtn.disabled = true;
+
+
+function startBtnClick() {
+    console.log('клик');
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+ timerId = setInterval(() => {
+    bodyEl.style.background = getRandomHexColor();
+  }, 1000)
+};
+
+
+function stopBtnClick() {
+    console.log('клик stop');
+  clearInterval(timerId);
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+
+};
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
